b06c96b1065baea49d14475504d27983378c869c
Hijtec <kulichusik@seznam.cz>
2020-03-21 19:17:46 +0100

    huge commit with functions :D

---
+++ stage/but_postprocess.py
@@ -0,0 +1,116 @@
+#############
+#Imports
+import numpy as np
+from math import floor as rounddown
+#Input: Raw detections of buttons
+#Output: Proposed corrected numpy array of elevator panel
+#Description: 
+#Requisities: all labeled buttons were detected, buttons are ordered from bottom to top
+#Notes:
+#############
+dtype = [('x', float),( 'y', float),( 'n', int)]
+data = np.array([(0.625,0.11,1),
+                (0.12,0.35,2),(0.39,0.36,3),(0.61,0.38,4),(0.89,0.32,5),
+                (0.14,0.53,6),(0.40,0.52,7),(0.62,0.56,8),(0.90,0.58,9),
+                (0.11,0.72,10),(0.35,0.71,11),(0.58,0.74,12),(0.82,0.71,13),
+                (0.10,0.93,14),(0.37,0.88,15)],
+                dtype=dtype)
+but_w = 0.25
+but_h = 0.2
+
+class Detection:
+    def __init__(self,detected,but_w,but_h):
+        self.detected = detected
+        self.buttons = []
+        self.but = (but_w,but_h)
+        self.max_rows = rounddown(1/but_h)
+        self.max_cols = rounddown(1/but_w)
+        self.adj_cooef = 1
+    def create_button(self,detected):
+        for i in detected:
+            x_raw = i[0]
+            y_raw = i[1]
+            n_raw = i[2]
+            self.buttons.append(Button(x_raw,y_raw,n_raw))
+        return self.buttons
+    """
+    def create_dividers(self):
+        #obsolete
+        div_x = []
+        div_y = []
+        for i in range(0,self.max_rows):
+            div_y.append(self.but[1]*i + self.but[1]/2)
+        for i in range(0,self.max_cols):
+            div_x.append(self.but[0]*i + self.but[0]/2)
+        return (div_x, div_y)
+    """
+    def find_classes(self,axis):
+        if axis == "row":
+            axis = 1
+        else:
+            axis = 0
+        i = -1
+        sames = []
+        comp_val_history = []
+        for c in data:
+            i +=1
+            compare_val = data[i][axis]
+            same_class = []
+            j = -1
+            for d in data:
+                j +=1
+                val = d[axis]
+                if abs(val-compare_val) < self.but[axis]/2:
+                    same_class.append(j)
+                    compare_val = compare_val + (val-compare_val)/self.adj_cooef
+            sames.append(same_class)
+            comp_val_history.append(rounddown(compare_val*10))
+        sames = np.array(sames)
+        sames_unique = np.unique(sames)
+        return sames_unique, comp_val_history
+
+class Button:
+    def __init__(self,x_raw,y_raw,n_raw):
+        self.x_raw = x_raw
+        self.y_raw = y_raw
+        self.n_raw = n_raw
+        self.n_mis = 0
+        self.n_corr = None
+        self.col = None
+        self.row = None
+    
+class Panel:
+    def __init__(self,buttons,rows,cols):
+        self.buttons = buttons
+        #assign rows and cols
+        i = 0
+        for row in rows:
+            i +=1
+            for item in row:
+                buttons[item].row = i
+        j = 0
+        for col in cols:
+            j +=1
+            for item in col:
+                buttons[item].col = j
+    def assign_row_col(self,buttons,rows,cols):
+        i = 0
+        for row in rows:
+            i +=1
+            for item in row:
+                buttons[item].row = i
+        j = 0
+        for col in cols:
+            j +=1
+            for item in col:
+                buttons[item].col = j
+        return buttons
+
+det = Detection(data,but_w,but_h)
+rows, r_val_hist = det.find_classes("row")
+cols, c_val_hist = det.find_classes("column")
+print(np.array(r_val_hist),np.sort(np.array(c_val_hist)))
+#buttons = det.create_button(data)
+#panel = Panel(buttons,rows,cols)
+#for i in buttons:
+    #print(i.row, i.col)
--- stage/seq_order_test.py
+++ stage/seq_order_test.py
@@ -11,26 +11,49 @@ import numpy as np
 test_real = np.array([[2,5,8,11],[1,3,6,9,12],[4,7,10]])
 test = np.array([[2,5,8,11],[3,6,9,12],[4,7,10,13]])
 cols = len(test_real)
-rows = []
+rows_in_cols = []
 for col in range(0, cols):
     row = np.size(test_real[col])
-    rows.append(row)
-rows = np.array(rows)
-print(rows)
-
-class Template:
-    def __init__(self,rows,cols,lenght):
-        # Initialize templates
-        self.temp_left_right_up = np.array([])
-        self.temp_right_left_up = np.array([])
-        self.temp_up_left_right = np.array([])
-        self.temp_up_right_left = np.array([])
+    rows_in_cols.append(row)
+    print(rows_in_cols)
+rows_in_cols = np.array(rows_in_cols)
+l=1
+print(test_real[1:-1*l])
+class Panel:
+    def __init__(self):
+        self.x_length = None
+        self.y_length = 0
     
-    def create(self, foo):
-        #cols may not be stable
+    def size(self, foo):
+        self.x_length = len(foo)
+        for col in range(0,cols):
+            if np.size(foo[col]) > self.y_length:
+                self.y_length = np.size(foo[col])
+
+class Button:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.number = None
+    
+    def coord(self, x_length, y_length, foo):
         self.cols = len(foo)
-        self.rows = []
-        for col in range(0, cols):
-            row = len(test_real[col])
-            self.rows.append(row)
-        self.vect = np.array(self.rows)
+        self.x_length = x_length
+        self.y_length = y_length
+        rows_in_cols = []
+        for col in range(0, self.cols):
+            rows = np.size(test_real[col])
+            rows_in_cols.append(row)
+            print(rows_in_cols)
+        rows_in_cols = np.array(rows_in_cols)
+        self.leftcol = rows_in_cols[0]
+        self.rightcol = rows_in_cols[-1]
+        self.midcol = rows_in_cols[1:-1]
+        for col in self.midcol:
+            if col > self.leftcol and col > self.rightcol:
+                col_norm = foo[-1]
+
+            
+panel = Panel()
+panelsize = panel.size(test_real)
+#print(panel.x_length, panel.y_length)
