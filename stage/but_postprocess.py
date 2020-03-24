f7b2b90846c07588c7ed01f8887cd1bab869be7d
Hijtec <kulichusik@seznam.cz>
2020-03-24 19:52:51 +0100

    changes to button classification + new template methods

--- stage/but_postprocess.py
+++ stage/but_postprocess.py
@@ -44,14 +44,14 @@ class Detection:
 
     Attributes:
         detected:                   An array of given data from recognition
-        buttons:                    List of Button classes based on given data
+        buttons_raw:                List of Button classes based on given data
+        template:                   Template object, corrects button sequence
         panel:                      Panel object
-        template:                   Template object
         but:                        List of detected button parameters
         adj_cooef: TODO: A cooeficient for moving the comparison value for imperfect/real positions
 
     Methods:
-        create_buttons:             Creates a list of Button objects
+        create_buttons_raw:         Creates a list of Button objects from raw data
         create_panel:               Creates a slave Panel object
         create_template:            Creates a slave Template object
         find_classes(axis = 1/0):   Finds buttons along the same row/column
@@ -62,39 +62,41 @@ class Detection:
     def __init__(self,detected,but_w,but_h):
         """Initializes the class and calls its methods."""
         self.detected = detected
-        self.buttons = None
+        self.buttons_raw = None
+        self.template = None
         self.panel = None
         self.but = (but_w,but_h)
         self.adj_cooef = 1
 
-        self.create_buttons() 
-        self.create_panel()
+        self.create_buttons_raw()
         self.create_template()
-
-    def create_buttons(self):
-        """Creates a list of button objects."""
+        self.create_panel()
+        
+    def create_buttons_raw(self):
+        """Creates a list of button objects based on raw data."""
         button_list = []
         for i in self.detected:
             x_raw, y_raw, n_raw = i[0], i[1], i[2]
             button_list.append(Button(x_raw,y_raw,n_raw))
-        self.buttons = button_list
-    
-    def create_panel(self):
-        """Creates a panel object with all of its necessities."""
+        self.buttons_raw = button_list
+
+    def create_template(self):
+        """Creates a template object."""
         rows, rows_all, r_val_hist = self.find_classes("row")
         cols, cols_all, c_val_hist = self.find_classes("col")
 
         if len(rows) > 1/but_h: raise ValueError("There can't be more rows than can physically fit into statespace")
         if len(cols) > 1/but_w: raise ValueError("There can't be more cols than can physically fit into statespace")
 
-        rows_ordered = self.order_unique_coord(rows_all, r_val_hist, "row")
-        cols_ordered = self.order_unique_coord(cols_all, c_val_hist, "col")
+        self.rows_ordered = self.order_unique_coord(rows_all, r_val_hist, "row")
+        self.cols_ordered = self.order_unique_coord(cols_all, c_val_hist, "col")
 
-        self.panel = Panel(self.buttons, rows_ordered, cols_ordered)
+        self.template = Template(self.buttons_raw, self.rows_ordered, self.cols_ordered)
+    
+    def create_panel(self):
+        """Creates a panel object with all of its necessities."""
 
-    def create_template(self):
-        """Creates a template object."""
-        self.template = Template(self.panel)
+        #self.panel = Panel(self.template.buttons_ordered, rows_ordered, cols_ordered)
 
     def find_classes(self,axis):
         """Finds rows/columns in data.
@@ -175,51 +177,6 @@ class Detection:
         
         return out
 
-class Button:
-    """A storage class for button instance.
-
-    Attributes:
-        x_raw:                  X coordinate from button recognition data
-        y_raw:                  Y coordinate from button recognition data
-        n_raw:                  Proposed number from button recognition data
-        n_miss:                 An attribute indicating whether the button is suspected to be mislabeled
-        col:                    Column coordinate/position
-        row:                    Row coordinate/position
-    """
-    def __init__(self,x_raw,y_raw,n_raw):
-        """Initializes the class with position parameters."""
-        self.x_raw = x_raw
-        self.y_raw = y_raw
-        self.n_raw = n_raw
-        self.n_miss = 0
-        self.n_corr = None
-        self.col = None
-        self.row = None
-    
-class Panel:
-    """A class for assigning buttons coords to a detection instance.
-
-    Attributes:
-        buttons:               List of Button instances
-        rows:                  List of ordered unique rows
-        cols:                  List of ordered unique columns
-    """
-    def __init__(self,buttons,rows,cols):
-        self.buttons = buttons
-        self.rows = rows
-        self.cols = cols
-        #assign rows and cols
-        i = 0
-        for row in rows:
-            i +=1
-            for item in row:
-                self.buttons[item].row = i
-        j = 0
-        for col in cols:
-            j +=1
-            for item in col:
-                self.buttons[item].col = j
-
 class Template:
     """A class for assigning a template to detection instance.
 
@@ -229,7 +186,8 @@ class Template:
         priority_vh:                Boolean of horizontal-vertical sequence priority
         rows:                       List of ordered unique cols inherited from panel instance
         cols:                       List of ordered unique rows inherited from panel instance
-        panel:                      Panel inherited instance
+        seq:                        Sequence of raw button numbers
+        jump_button:
     
     Methods:
         find_template_candidate:    Finds ranks of all possible templates and saves them in a list
@@ -238,16 +196,25 @@ class Template:
         count_vh:                   Creates an iterable list through which it computes horizontal-vertical ranks
         suppress_odd_rows:          Based on average members in row suppresses first row, prints info if it did
         recalculate_cols:           Recalculates the columns based upon the suppressed rows
+        flatten_sqq:                Flattens number sequence (suppreses odd rows for priority_vh = False)
+        find_seq_error              Finds errors in numbering buttons
+
+        TODO: fix sequence:         Fixes the number sequence
+        TODO: order buttons:        Creates an ordered list of button objects for further use
     """
-    def __init__(self,panel):
+    def __init__(self, buttons_raw, rows_ordered, cols_ordered):
         self.n_ranks = None
         self.priority_lr = None #True if left->right, false if right->left
         self.priority_vh = None #True if counting by rows, false if counting by columns
-        self.rows = panel.rows
-        self.cols = panel.cols
-        self.panel = panel
+        self.rows = rows_ordered
+        self.cols = cols_ordered
+        self.seq = None
+        self.buttons_raw = buttons_raw
+
         self.find_template_candidate()
         self.assign_template()
+        self.flatten_seq()
+        self.find_seq_error()
     
     def find_template_candidate(self):
         """Ranks the numbering template based upon its correct probability."""
@@ -305,7 +272,7 @@ class Template:
         listed_numbers = []
         for row in self.rows:
             for i in row:
-                listed_numbers.append(self.panel.buttons[i].n_raw) #get the proposed button number
+                listed_numbers.append(self.buttons_raw[i].n_raw) #get the proposed button number
         
         #Defining starting positions
         if axis == 1:
@@ -345,14 +312,14 @@ class Template:
         else:
             raise ValueError("Order must be either left or right")
         #Suppression and recalculation of odd data
-        rows_suppressed = self.suppress_odd_rows()
+        rows_suppressed,___ = self.suppress_odd_rows()
         cols_suppressed = self.recalculate_cols(rows_suppressed)
         #Making an iterable list to count with
         listed_numbers = []
 
         for col in cols_suppressed:
             for i in col:
-                listed_numbers.append(self.panel.buttons[i].n_raw) #get the proposed button number
+                listed_numbers.append(self.buttons_raw[i].n_raw) #get the proposed button number
 
         #Defining starting positions
         if axis == 1:
@@ -392,7 +359,7 @@ class Template:
         else:
             suppressed_rows = self.rows
 
-        return suppressed_rows
+        return suppressed_rows, del_index
 
     def recalculate_cols(self,suppressed):
         """Recalculates columns after the potential suppression of first row.
@@ -411,26 +378,152 @@ class Template:
                 ncols = len(i)
         
         for i in range(ncols):
-            #TODO:get rid of try i 0,1,2,3
-            """
-            try:
-                col = []
-                for item in suppressed:
-                    col.append(item[i])
-                cols_ordered_suppressed.append(col)
-            except:
-                cols_ordered_suppressed.append(col)
-            """
             col = []
             for item in suppressed:
-                print(len(item), i)
                 if len(item)-1 >= i:
                     col.append(item[i])
             cols_ordered_suppressed.append(col)
-            print(cols_ordered_suppressed)
     
         return cols_ordered_suppressed
 
+    def flatten_seq(self):
+        """Creates an iterable list based on assigned template."""
+        seq = []
+        if self.priority_vh == True:
+            if self.priority_lr == True:
+                rows_private = self.rows
+            
+            elif self.priority_lr == False:
+                rows_private = []
+                for row in self.rows:
+                    rows_private_row = []
+                    flip = -1
+                    for _ in range(len(row)):
+                        rows_private_row.append(row[flip])
+                        flip -=1
+                    rows_private.append(rows_private_row)
+            else:
+                raise ValueError("Priorities have to be boolean")
+                    
+            for row in rows_private:
+                curr = 0
+                for _ in row:
+                    seq.append(self.buttons_raw[row[curr]].n_raw)
+                    curr += 1
+            
+            del_index = []
+                    
+        elif self.priority_vh == False:
+            rows_suppressed, del_index = self.suppress_odd_rows()
+            cols_suppressed = self.recalculate_cols(rows_suppressed)
+
+            if self.priority_lr == True:
+                cols_private = cols_suppressed
+            
+            elif self.priority_lr == False:
+                cols_private = []
+                flip = -1
+                for __ in cols_suppressed:
+                    cols_private.append(cols_suppressed[flip])
+                    flip -= 1
+            else:
+                raise ValueError("Priorities have to be boolean")
+
+            for col in cols_private:
+                curr = 0
+                for _ in col:
+                    seq.append(self.buttons_raw[col[curr]].n_raw)
+                    curr += 1
+        else:
+                raise ValueError("Priorities have to be boolean")
+        self.seq = seq
+        self.del_flatten_index = del_index
+        return seq
+
+    def find_seq_error(self):
+        """Based on teplate, finds out which numbers are wrong."""
+        seq = self.seq
+        seq_correct = []
+        if seq[0] == seq[-1]-len(seq)+1 or seq[0] == seq[1]-1:
+            seq_correct.append(True)
+        else:
+            seq_correct.append(False)
+    
+        for i in range(len(seq)-2):
+            print(i)
+            if seq[i+1] == seq[i]+1 or seq[i+1] == seq[i+2]-1:
+                seq_correct.append(True)
+            else:
+                seq_correct.append(False)
+
+        if seq[-1] == seq[-2]+1 or seq[-1] == seq[0]+len(seq)-1:
+            seq_correct.append(True)
+        else:
+            seq_correct.append(False)
+        
+        self.seq_correct = seq_correct
+        return seq, seq_correct
+
+
+
+
+class Button:
+    """A storage class for button instance.
+
+    Attributes:
+        x_raw:                  X coordinate from button recognition data
+        y_raw:                  Y coordinate from button recognition data
+        n_raw:                  Proposed number from button recognition data
+        n_miss:                 An attribute indicating whether the button is suspected to be mislabeled
+        col:                    Column coordinate/position
+        row:                    Row coordinate/position
+    """
+    def __init__(self,x_raw,y_raw,n_raw):
+        """Initializes the class with position parameters."""
+        self.x_raw = x_raw
+        self.y_raw = y_raw
+        self.n_raw = n_raw
+        self.n_miss = 0
+        self.n_corr = None
+        self.col = None
+        self.row = None
+ 
+class Panel:
+    """A class for button panel.
+
+    Attributes:
+        buttons:                List of Button instances
+        rows:                   List of ordered unique rows
+        cols:                   List of ordered unique columns
+        priority_lr:            Boolean of left-right sequence priority
+        priority_vh:            Boolean of horizontal-vertical sequence priority
+    
+    Methods:
+        assign_buttons:         Assigns coordinates of button objects
+        
+    """
+    def __init__(self,buttons,rows,cols,priority_lr,priority_vh):
+        self.buttons = buttons
+        self.rows = rows
+        self.cols = cols
+        self.priority_lr = priority_lr
+        self.priority_vh = priority_vh
+        self.assign_buttons()
+    
+    def assign_buttons(self):
+        """A method for assigning buttons coords to a detection instance."""
+        i = 0
+        for row in rows:
+            i +=1
+            for item in row:
+                self.buttons[item].row = i
+        j = 0
+        for col in cols:
+            j +=1
+            for item in col:
+                self.buttons[item].col = j
+
 #Istance of Detection class
 det = Detection(data_OCR,but_w,but_h)
-print(det.template.n_ranks,det.template.priority_lr,det.template.priority_vh, det.template.rows, det.template.cols, det.panel.buttons[5].n_raw)
+#print(det.template.n_ranks,det.template.priority_lr,det.template.priority_vh, det.template.rows, det.template.cols, det.buttons_raw[5].n_raw)
+print(det.cols_ordered, det.template.seq_correct)
